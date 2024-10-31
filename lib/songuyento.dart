99a3ae57094ce84a512c71c14ca68e23c7b6229d
Nhan ngo <[ngo726278@gmail.com]>
2024-10-31 15:25:41 +0700

    Initial commit

--- lib/songuyento.dart
+++ lib/songuyento.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(PrimeCheckerApp());
 
-class PrimeCheckerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PrimeCheckerScreen(),
     );
   }
 }
 
-class PrimeCheckerScreen extends StatefulWidget {
   @override
-  _PrimeCheckerScreenState createState() => _PrimeCheckerScreenState();
 }
 
-class _PrimeCheckerScreenState extends State<PrimeCheckerScreen> {
-  final TextEditingController _numberController = TextEditingController();
-  String _result = '';
 
   // Hàm kiểm tra số nguyên tố
-  bool _isPrime(int number) {
     if (number < 2) return false;
     for (int i = 2; i <= number ~/ 2; i++) {
       if (number % i == 0) return false;
@@ -29,18 +20,8 @@ class _PrimeCheckerScreenState extends State<PrimeCheckerScreen> {
     return true;
   }
 
-  // Hàm xử lý sự kiện kiểm tra số
-  void _checkPrime() {
-    int? number = int.tryParse(_numberController.text);
-    setState(() {
-      if (number == null) {
-        _result = 'Vui lòng nhập một số hợp lệ';
-      } else if (_isPrime(number)) {
-        _result = 'Số $number là số nguyên tố';
-      } else {
-        _result = 'Số $number không phải là số nguyên tố';
-      }
-    });
+    if (number == null) {
+    }
   }
 
   @override
@@ -52,26 +33,15 @@ class _PrimeCheckerScreenState extends State<PrimeCheckerScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // TextField để nhập số
             TextField(
-              controller: _numberController,
               decoration: InputDecoration(
                 labelText: 'Nhập một số',
                 border: OutlineInputBorder(),
               ),
-              keyboardType: TextInputType.number, // Chỉ cho phép nhập số
             ),
             SizedBox(height: 20),
             // Nút kiểm tra
             ElevatedButton(
-              onPressed: _checkPrime,
-              child: Text('Kiểm Tra'),
-            ),
-            SizedBox(height: 20),
-            // Text hiển thị kết quả
-            Text(
-              _result,
-              style: TextStyle(fontSize: 24),
             ),
           ],
         ),
